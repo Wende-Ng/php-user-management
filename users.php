0d82c0d6e10a57729f66b69f96b52f4e61139ea4
Wende.Ng <w.ng@gmx.de>
2020-07-14 14:37:51 +0200

    Addition of new columns

--- users.php
+++ users.php
@@ -6,22 +6,37 @@
 $users = [
     [
         'username' =>'Alex',
+        'Vorname' =>' Alex',
+        'Nachname' =>'Schmidt',
+        'e_mail' => 'alex@mail.de',
         'password' => 'hallo'
     ],
     [
         'username' => 'Tim',
+        'Vorname' =>' Tim',
+        'Nachname' =>'Müller',
+        'e_mail' => 'tim@mail.de',
         'password' => 'Desktop'
     ],
     [
         'username' => 'Bernd',
+        'Vorname' =>' Bernd',
+        'Nachname' =>'his',
+        'e_mail' => 'bernd@mail.de',
         'password' => 'Keyboard'
     ],
     [
         'username' => 'andreas',
+        'Vorname' =>' Andreas',
+        'Nachname' =>'Dorf',
+        'e_mail' => 'andreas@mail.de',
         'password' => 'bild'
     ],
     [
         'username' => 'hans',
+        'Vorname' =>' hans',
+        'Nachname' =>'kimmel',
+        'e_mail' => 'kimmel@mail.de',
         'password' => 'tee'
     ]
 ];
