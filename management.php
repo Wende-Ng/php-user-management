4993cf164b377cc6abbb93e67d349d259e5f5f4c
Wende.Ng <w.ng@gmx.de>
2020-07-15 11:52:36 +0200

    Added the ability to change the User

--- management.php
+++ management.php
@@ -6,20 +6,37 @@ session_start();
 $db = new PDO('mysql:host=localhost;  dbname=app', 'app', 'app');
 $statement = $db->query("SELECT * FROM user");
 $user = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-foreach ($user as $value){
-    $username = $value['username'];
-    $Vorname = $value['Vorname'];
-    $Nachname = $value['Nachname'];
-    $e_mail = $value['e_mail'];
-    $id = $value['id'];
-    echo "$username ";
-    echo "$Vorname ";
-    echo "$Nachname ";
-    echo "$e_mail ";
-    echo "$id <br \>";
+if(!isset($_SESSION['username'])){
+    header("Location: /index.php");
+    die();
 }
 ?>
 
-
+<div >
+    <div style="width: 100%; display: inline-block"  align="right">
+    <p > Username: <?php echo $_SESSION['username']; ?> </p>
+    <form  method="post">
+        <input type="submit" name="logout" value="logout"/>
+    </form>
+    </div>
+    <div style="width: 50%; display: inline-block" align="left">
+    <?php
+    foreach ($user as $value): ?>
+        <a href="userEditor.php?id= <?php
+        echo $value['id']; ?>">Edit</a>
+        <?php
+        echo $value['username'], "\n", $value['Vorname'], "\n", $value['Nachname'], "\n", $value['e_mail'], "<br \>"; ?>
+    <?php
+    endforeach; ?>
+    <?php
+    if(isset($_POST[logout])){
+        session_destroy();
+        header("Location: /index.php");
+    }
+    ?>
+    </div>
+    <form action= "addUser.php" method="post">
+        <input type="submit" name="new" value="add user"/>
+    </form>
+</div>
 </html>
