9bfa0cdbf28eb6720cb99ac015b103882bbb405e
Wende.Ng <w.ng@gmx.de>
2020-07-17 11:26:53 +0200

    Organizing the file to conform the psr12 standard

--- actions/management.php
+++ actions/management.php
@@ -8,36 +8,31 @@ $db = connect();
 $statement = $db->query("SELECT * FROM user");
 $user = $statement->fetchAll(PDO::FETCH_ASSOC);
 if(!isset($_SESSION['username'])){
-    header("Location: /user-management/index.php");
+    header("Location: /user-management/actions/login");
     die();
 }
 ?>
 
-<div >
-    <div style="width: 100%; display: inline-block"  align="right">
-    <p > Username: <?php echo $_SESSION['username']; ?> </p>
+
+    <div  align="right">
+    <p > Username: <?= $_SESSION['username']; ?> </p>
     <form  method="post">
         <input type="submit" name="logout" value="logout"/>
     </form>
     </div>
-    <div style="width: 50%; display: inline-block" align="left">
     <?php
     foreach ($user as $value): ?>
-        <a  href="users/edit.php?id=<?php
-        echo escape($value['id']);?>">Edit</a>
-        <?php
-        echo $value['username'], "\n", $value['Vorname'], "\n", $value['Nachname'], "\n", $value['e_mail'], "<br \>"; ?>
-    <?php
-    endforeach; ?>
+        <a  href="users/edit.php?id=<?= escape($value['id']);?>">Edit</a>
+        <?= $value['username'], "\n", $value['Vorname'], "\n", $value['Nachname'], "\n", $value['e_mail'], "<br \>"; ?>
+    <?php endforeach; ?>
     <?php
     if(isset($_POST['logout'])){
         session_destroy();
-        header("Location: /user-management/index.php");
+        header("Location: /user-management/actions/login");
     }
     ?>
-    </div>
+
     <form action= "users/add.php" method="post">
         <input type="submit" name="new" value="add user"/>
     </form>
-</div>
 </html>
