3c7fc384fff11f1d3b1f47ae7ddb8a50ba942c10
Wende.Ng <w.ng@gmx.de>
2020-07-17 11:29:13 +0200

    Using index.php as the access point of the whole project

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
-<html>
-
-<head>
-</head>
-<body>
-<form action="actions/login.php" method="post">
-    <input type="submit" value="anmelden"/>
-</form>
-</body>
 <?php
 require_once __DIR__ . '/src/functions.php';
+$link = $_SERVER['REDIRECT_URL'];
+$query = $_SERVER['QUERY_STRING'];
+$path = str_replace("/user-management/", "", $link);
+if(file_exists($path . ".php" )){
+    require_once __DIR__ .'/'.$path . '.php' ;
+}
+else{
+    http_response_code(404);
+    include('404-not-found.php');
+    die();
+}
 
 
 ?>
-</html>
