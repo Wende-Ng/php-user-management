a911bf61a4eee0fd62a6b28c277fbfa3e71a9e29
Wende.Ng <w.ng@gmx.de>
2020-07-17 11:23:49 +0200

    Organizing the file to conform the psr12 standard

--- actions/users/edit.php
+++ actions/users/edit.php
@@ -4,8 +4,8 @@
 session_start();
 require_once __DIR__ . '/../../src/functions.php';
 require_once __DIR__ . '/../../src/User.php';
-if(!isset($_SESSION['username'])){
-    header("Location: /user-management/index.php");
+if (!isset($_SESSION['username'])) {
+    header("Location: /user-management/actions/login");
     die();
 }
 $db = connect();
@@ -15,45 +15,46 @@ $user = User::findById($db, $id);
 ?>
 <form method="post">
     Username:<br>
-    <input type="text" name="Username" value="<?php echo $user->getUsername();?>"/><br>
+    <input type="text" name="Username" value="<?= $user->getUsername(); ?>"/><br>
     Vorname:<br>
-    <input type="text" name="Vorname"value="<?php echo $user->getVorname();?>"/><br><br>
+    <input type="text" name="Vorname" value="<?= $user->getVorname(); ?>"/><br><br>
     Nachname:<br>
-    <input type="text" name="Nachname" value="<?php echo $user->getNachname();?>"/><br><br>
+    <input type="text" name="Nachname" value="<?= $user->getNachname(); ?>"/><br><br>
     E-mail:<br>
-    <input type="email" name="e_mail"value="<?php echo $user->getEMail();?>"/><br><br>
+    <input type="email" name="e_mail" value="<?= $user->getEMail(); ?>"/><br><br>
     <input type="submit" name="edit" value="save"/>
 </form>
-<p align="right"> Username: <?php echo $_SESSION['username']; ?> </p>
+<p align="right"> Username: <?= $_SESSION['username']; ?> </p>
 <form method="post">
     <input type="submit" name="delete" value="delete"/>
     <input type="submit" name="abort" value="abort"/>
-    <div align="right"><input type="submit" name="logout" value="logout"/> </div>
+    <div align="right"><input type="submit" name="logout" value="logout"/></div>
 </form>
 <?php
 if (isset($_POST['delete'])) {
     $user->delete($db);
-    header("Location: /user-management/actions/management.php");
+    header("Location: /user-management/actions/management");
 }
-if (isset($_POST['edit'])
+if (
+    isset($_POST['edit'])
     && isset($_POST['Username'])
     && isset($_POST['Vorname'])
     && isset($_POST['Nachname'])
-    && isset($_POST['e_mail'])) {
+    && isset($_POST['e_mail'])
+) {
     $user->setUsername($_POST['Username']);
     $user->setVorname($_POST['Vorname']);
     $user->setNachname($_POST['Nachname']);
     $user->setEMail($_POST['e_mail']);
     $user->save($db);
-    header("Location: /user-management/actions/management.php");
-
+    header("Location: /user-management/actions/management");
 }
 if (isset($_POST['abort'])) {
-    header("Location: /user-management/actions/management.php");
+    header("Location: /user-management/actions/management");
 }
-if(isset($_POST['logout'])){
+if (isset($_POST['logout'])) {
     session_destroy();
-    header("Location: /user-management/index.php");
+    header("Location: /user-management/index");
 }
 ?>
 
